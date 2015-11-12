2c20cf70abe90231ef354527cb700dddc1628d6c
Sanga <Sanga@v1020-wn-96-238.campus-dynamic.uwaterloo.ca>
2015-11-11 19:54:47 -0500

    AudioTrackUp

--- app/src/main/java/com/android/walkietalkie/AudioReceiverManager.java
+++ app/src/main/java/com/android/walkietalkie/AudioReceiverManager.java
@@ -1,14 +1,22 @@
 package com.android.walkietalkie;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+
 /**
  * Created by Habeeb Ahmed on 11/11/2015.
  */
 public class AudioReceiverManager implements ServerClient.IncomingAudioListener {
     private ServerClient mServerClient;
+    private AudioTrack mAudioTrack;
 
     public AudioReceiverManager(ServerClient serverClient) {
         mServerClient = serverClient;
+        mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 8000, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, 800000, AudioTrack.MODE_STREAM);
         registerIncomingAudioListener();
+        mAudioTrack.play();
     }
 
     // register and incoming audio listener with the mServerClient.
@@ -19,6 +27,6 @@ public class AudioReceiverManager implements ServerClient.IncomingAudioListener 
 
     @Override
     public void onIncomingAudio(byte[] data) {
-
+        mAudioTrack.write(data, 0, data.length);
     }
 }
